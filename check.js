df81714e6555f1e8db474cc2c1fc7d20d291730a
Kuppa Maksim <71605089+Craven4ik@users.noreply.github.com>
2021-12-06 18:52:15 +0300

    Add files via upload

---
+++ check.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.btn').click(function (event) {
+        $('.content3').toggleClass('active');
+    });
+    $('button').click(function () {
+        $('b').toggle();
+    });
+});
